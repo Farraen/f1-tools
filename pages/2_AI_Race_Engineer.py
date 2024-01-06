445f705b2064ad03da3b1e406c263b8edf917bc2
farraen”git config --global user.email “farraen@gmail.com <farraen@gmail.comgit config --global user.email farraen@gmail.com>
2024-01-06 15:47:47 +0000

    update

--- pages/2_AI_Race_Engineer.py
+++ pages/2_AI_Race_Engineer.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-
+from openai import OpenAI
+import streamlit as st
 import time
+
 
 # --------  For page layout  ---------------
 st.set_page_config(layout="wide")
@@ -40,9 +42,58 @@ def read_image(img_path):
     image = np.array(im)
     return image
 
+# OpenAI 
+@st.cache_resource 
+def connect_openAI():
+    openai_api_key = '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'
+    openai_client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key=openai_api_key,
+    )
+    return openai_client
+
+
+
+client = connect_openAI()
+
+
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+#with st.expander('Chat',expanded=True)
+
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 
 
 
+#st.write('Copyright © 2024 Farraen. All rights reserved.')
 
-st.write('Copyright © 2024 Farraen. All rights reserved.')
