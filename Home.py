76e418111d6b46aa97c19e433ccdbc9959709da6
Farraen <52446668+Farraen@users.noreply.github.com>
2024-04-15 14:32:55 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -94,5 +94,5 @@ with col12:
     image = read_image("images/Image_4.png")
     st.image(image)
     st.subheader('Tire Strategy')
-    st.write('A tool for testing tire strategy decision engine for planning tire change during a race.')
+    st.write('A tool for testing tire strategy decision engine for planning tire change during a race. Genetic algorithm is used as the decision engine to generate tire change solutions.')
     
