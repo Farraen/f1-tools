a6bd3baff9263d35c087b090af994e1753a48361
farraen”git config --global user.email “farraen@gmail.com <farraen@gmail.comgit config --global user.email farraen@gmail.com>
2025-09-20 02:10:58 +0100

    update

--- Home.py
+++ Home.py
@@ -4,7 +4,21 @@ from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
 
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",initial_sidebar_state="collapsed")
+
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+
 
 # For loading images
 @st.cache_resource
