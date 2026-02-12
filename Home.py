77a8d59dde97e7d69fab020c8bb64fb8b46e60bc
Farraen <52446668+Farraen@users.noreply.github.com>
2026-02-12 16:02:14 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -45,7 +45,7 @@ tools_data = [
         'page': 'PU Optimisation',
         'image': 'images/Image_1.png',
         'title': 'PU Selection Decision Engine',
-        'description': 'A decision engine designed to optimize F1 power unit (PU) selection for the entire season. This engine utilizes a genetic algorithm as the optimizer and incorporates an artificial damage model to assess vehicle performance degradation.',
+        'description': 'A decision engine designed to optimize F1 power unit (PU) selection for the entire season. This engine utilizes a genetic algorithm as the optimizer and incorporates an artificial damage model to assess vehicle performance degradation.x',
         'category': 'Strategy & Optimization'
     },
     {
@@ -139,39 +139,138 @@ tools_data = [
         'description': 'Variational Inference is a powerful technique to train any model or for parameterisation of engineering systems. It uses variational distribution to approximate the posterior distribution of the model parameters. It is useful when the training data is too sparse or too complex for parameterisation The output of the process is the optimised parameters as well as the uncertainty of the parameters.',
         'category': 'Optimisation'
     },
+tools_data = [
+    {
+        'page': 'PU Optimisation',
+        'image': 'images/Image_1.png',
+        'title': 'PU Selection Decision Engine',
+        'description': 'A decision engine designed to optimise F1 power unit (PU) selection for the entire season. This engine utilises a genetic algorithm as the optimiser and incorporates an artificial damage model to assess vehicle performance degradation.',
+        'category': 'Strategy & Optimization'
+    },
+    {
+        'page': 'PU Optimisation LLM',
+        'image': 'images/Image_2.png',
+        'title': 'PU Decision Engine + AI Race Engineer',
+        'description': 'The same PU decision engine dashboard is now powered by a Large Language Model (LLM). It provides recommendations based on the current PU strategy situation. The LLM persona is tuned to mimic a junior race engineer.',
+        'category': 'Strategy & Optimization'
+    },
+    {
+        'page': 'EV Race Car Optimiser',
+        'image': 'images/Image_12.png',
+        'title': 'EV Race Car Optimiser',
+        'description': 'A tool for optimising EV race car parameters to achieve the most efficient lap times.',
+        'category': 'Optimisation'
+    },
+    {
+        'page': 'Lap Time Prediction',
+        'image': 'images/Image_11.jpg',
+        'title': 'Lap Time Predictor',
+        'description': 'A tool for predicting vehicle lap times over a circuit using machine learning.',
+        'category': 'Strategy & Optimization'
+    },
+    {
+        'page': 'Combustion Strategy',
+        'image': 'images/Image_4.png',
+        'title': 'Model Parameter Tuner',
+        'description': 'A tool for parameterising models (such as combustion or exhaust temperature models) for sensorless systems. It is a simple tool for filling in model parameters to match measured data from a physical sensor.',
+        'category': 'Optimisation'
+    },
+    {
+        'page': 'Generative Design',
+        'image': 'images/Image_6.png',
+        'title': 'Generative Design',
+        'description': 'A decision engine demonstrator for assisting engineers in powertrain design selection, using AI and machine learning models to make fast decisions based on user design criteria.',
+        'category': 'Generative Design'
+    },
+    {
+        'page': 'Tire Strategy',
+        'image': 'images/Image_7.png',
+        'title': 'Tire Strategy',
+        'description': 'A decision engine platform for tyre change strategy. It helps race engineers predict the optimal lap for tyre changes and compound selection. A genetic algorithm is used as the decision engine to generate tyre change solutions.',
+        'category': 'Strategy & Optimization'
+    },
+    {
+        'page': 'Prognostics',
+        'image': 'images/Image_8.png',
+        'title': 'Prognostics',
+        'description': 'A simple dashboard for investigating early failures using race telemetry data.',
+        'category': 'Anomaly Detection'
+    },
+    {
+        'page': 'Data Mining',
+        'image': 'images/Image_10.png',
+        'title': 'Data Mining (Under development)',
+        'description': 'A data exploration tool for discovering underlying patterns and trends.',
+        'category': 'Data Analysis'
+    },
+    {
+        'page': 'Model Based Calibration',
+        'image': 'images/Image_3.png',
+        'title': 'Model Based Calibration Methodology',
+        'description': '(Under development) Model-based Calibration is a process for optimally tuning system parameters. It involves creating a Design of Experiments (DoE), developing models, and performing optimisation.',
+        'category': 'Optimisation'
+    },
+    {
+        'page': 'Anomaly Estimation',
+        'image': 'images/Image_5.png',
+        'title': 'Anomaly Detection Testing Tool',
+        'description': 'A tool for training a machine learning classifier to detect anomalies in vehicle measurement data.',
+        'category': 'Anomaly Detection'
+    },
+    {
+        'page': 'Other Ways of Visualising Data',
+        'image': 'images/big_data.png',
+        'title': 'Data Visualisation',
+        'description': 'A page dedicated to new ways of exploring complex and large datasets to extract useful insights. It helps uncover hidden patterns and relationships in a more intuitive manner. As a starting point, it combines time-series segmentation, prompt engineering, and knowledge graphs to quickly visualise factors affecting lap time. This is still a work in progress, but sufficient as a proof of concept.',
+        'category': 'Data Analysis'
+    },
+    {
+        'page': 'Multi Task GP',
+        'image': 'images/mtgp_1.png',
+        'title': 'Multi Task GP Model',
+        'description': 'This app demonstrates the use of a multi-task GP framework to train a GP model from another GP model. The goal is to build a statistical model from sparse training data and leverage historical engine data from a similar engine.',
+        'category': 'Optimisation'
+    },
+    {
+        'page': 'Variational Inference',
+        'image': 'images/SVI.png',
+        'title': 'Variational Inference',
+        'description': 'Variational Inference is a powerful technique for training models and parameterising engineering systems. It uses a variational distribution to approximate the posterior distribution of model parameters. It is especially useful when training data is sparse or complex. The output of the process includes optimised parameters as well as their uncertainty.',
+        'category': 'Optimisation'
+    },
     {
         'page': 'Anomaly Detection Using Transformer',
         'image': 'images/big_data.png',
         'title': 'Anomaly Detection Using Transformer',
-        'description': 'A page dedicated for new ways of exploring complex and large data, and getting useful insights.It helps to find hidden patterns and relationships in more intuitive manner. For a start, I have combined time-series segmentation, prompt engineerng and knowledge graph to quickly visualise factors affecting a lap time. It is still work in progress but enough as proof of concept.',
+        'description': 'A page dedicated to new ways of exploring complex and large datasets to extract useful insights. It helps uncover hidden patterns and relationships in a more intuitive manner. As a starting point, it combines time-series segmentation, prompt engineering, and knowledge graphs to quickly visualise factors affecting lap time. This is still a work in progress, but sufficient as a proof of concept.',
         'category': 'Anomaly Detection'
     },
     {
         'page': 'Anomaly Detection And Insights',
         'image': 'images/Image_17.png',
         'title': 'Anomaly Detection and Insights',
-        'description': 'A page to demonstrate the ability of using foundation models to analyse data and provide insights and root cause analysis for an engine component. It uses fuel pump failure as an example and use AI to determine failure modes and factors contributing to the failure.',
+        'description': 'A page demonstrating the use of foundation models to analyse data and provide insights and root cause analysis for an engine component. It uses fuel pump failure as an example and applies AI to determine failure modes and contributing factors.',
         'category': 'Anomaly Detection'
     },
     {
         'page': 'https://farraen.github.io/streamlit_demo/',
         'image': 'images/stlite.png',
         'title': 'Streamlit without Python backend',
-        'description': 'A page to showcase the ability of using streamlit without Python backend. Works by loading Pyodide, Stlite and other libraries directly into browser.',
+        'description': 'A page showcasing the ability to use Streamlit without a Python backend. It works by loading Pyodide, Stlite, and other libraries directly in the browser.',
         'category': 'Data Analysis'
     },
     {
         'page': 'Interactive Report Generator',
         'image': 'images/interactive.png',
         'title': 'Interactive Report Generator',
-        'description': 'A tool for generating interactive reports for more intuitive experience compared to normal excel or pdf reports. This app generates a report showing an engine ML model performance.',
+        'description': 'A tool for generating interactive reports that provide a more intuitive experience compared to traditional Excel or PDF reports. This app generates a report showing engine ML model performance.',
         'category': 'Data Analysis'
     },
     {
         'page': 'Multi Agent',
         'image': 'images/multi_agent.jpg',
         'title': 'Multi Agent Demonstration',
-        'description': 'A tool for demonstrating the ability to orchestrate multiple agents to solve a problem.',
+        'description': 'A tool for demonstrating the orchestration of multiple agents to collaboratively solve a problem.',
         'category': 'Agentic AI'
     },
 ]
