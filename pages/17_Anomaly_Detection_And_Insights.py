2d2e623fb3772bfaaaa067ba6c814d6058b7459f
Farraen <52446668+Farraen@users.noreply.github.com>
2026-02-12 16:43:22 +0000

    Update 17_Anomaly_Detection_And_Insights.py

--- pages/17_Anomaly_Detection_And_Insights.py
+++ pages/17_Anomaly_Detection_And_Insights.py
@@ -658,4 +658,4 @@ with col2:
         else:
             st.write("Select a time window by dragging on the left side plot to start interrogating the signal. Pick those with anomalies.")
 
-st.write('Copyright © 2025 Farraen. All rights reserved.')
+st.write('Copyright © 2026 Dr Farraen. All rights reserved.')
